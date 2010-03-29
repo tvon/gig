39e2d9aec613fa6a513688342c719484f0d3450f
Tom von Schwerdtner <tomvons@gmail.com>
2010-03-29 12:42:19 -0400

    add some empty directories and improve copying feedback

--- gig/commands/init.py
+++ gig/commands/init.py
@@ -28,7 +28,7 @@ def copy_helper(template_dir, dest, name):
         else:
             message = 'use only numbers, letters, underscores and hyphens'
         raise CommandError("%r is not a valid project name. Please %s." % (name, message))
-        
+
     top_dir = os.path.abspath(dest)
     try:
         os.mkdir(top_dir)
@@ -41,20 +41,25 @@ def copy_helper(template_dir, dest, name):
     for d, subdirs, files in os.walk(template_dir):
         relative_dir = d[len(template_dir)+1:].replace(template, name)
         if relative_dir:
+            print "\tdir:\t%s/" % os.path.join(name, relative_dir)
             os.mkdir(os.path.join(top_dir, relative_dir))
 
-        # TODO: just strip .svn, .git, .hg, not .hgignore or .gitignore, 
-        # for example
+        # Ignore 'hidden' directories
         for i, subdir in enumerate(subdirs):
             if subdir.startswith('.'):
                 del subdirs[i]
 
         for f in files:
+
+            # A dummy file so we can have empty directories in git/hg
+            if f == '.empty':
+                continue
+
             path_old = os.path.join(d, f)
             path_new = os.path.join(top_dir, relative_dir, f.replace(template, name))
             fp_old = open(path_old, 'r')
             fp_new = open(path_new, 'w')
-            print "\tcreate\t%s" % os.path.join(name, relative_dir, f.replace(template, name))
+            print "\tfile:\t%s" % os.path.join(name, relative_dir, f.replace(template, name))
             fp_new.write(fp_old.read().replace('{{ GIG_PROJECT_NAME }}', name).replace('{{ GIG_PROJECT_HOME }}', dest))
             fp_old.close()
             fp_new.close()
@@ -83,43 +88,46 @@ class Command(BaseCommand):
 
     help = 'initialize new project'
     args = '[project_name]'
-    
+
     option_list = BaseCommand.option_list + (
         make_option('-l', '--list', help='List available project templates', action='store_true'),
         make_option('-t', '--template', help='Template to use, defaults to "django"'),
-        
     )
-    
+
     template_dir = '%s/templates/' % gig.__path__[0]
     template = 'django'
-    
+
     def get_templates(self):
         # TODO: Make this better...
         templates = os.listdir(self.template_dir)
         return templates
-        
+
     def handle_list(self):
         templates = self.get_templates()
         for t in templates:
             print t
-            
+
     def handle(self, *args, **options):
-        
+
         if options.get('list') == True:
             self.handle_list()
             sys.exit(0)
-        
+
         if options.get('template'):
             self.template = options.get('template')
+
+        if len(args) < 1:
+            sys.stderr.write('You must provide a destination project name\n')
+            sys.exit(1)
 
         # Make sure it exists...
         if not os.path.exists(self.template_dir + self.template):
             sys.stdout.write('Template for "%s" not found, please specify another template.\n' % self.template)
             sys.exit(1)
-            
+
         name = args[0]
         copy_helper(os.path.join(self.template_dir, self.template), os.path.abspath(name), name)
-        
+
         gigrc = os.path.join(self.template_dir, self.template, '.gigrc')
         gigrc_defaults = {'gig_project_home': os.path.abspath(name), 'gig_project_name': name}
         if os.path.exists(gigrc):
