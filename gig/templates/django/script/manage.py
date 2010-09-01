85a3b4f4cc956903c971d9c973e7d7d580b65c6b
Tom von Schwerdtner <tomvons@gmail.com>
2010-08-31 20:55:29 -0400

    whitespace

--- gig/templates/django/script/manage.py
+++ gig/templates/django/script/manage.py
@@ -2,7 +2,6 @@
 import sys, os
 
 PROJECT_HOME = os.path.dirname(os.path.abspath('%s/../' % __file__))
-
 sys.path[1:1] = ['%s/config' % PROJECT_HOME, '%s/app' % PROJECT_HOME, '%s/lib' % PROJECT_HOME]
 
 from django.core.management import execute_manager
